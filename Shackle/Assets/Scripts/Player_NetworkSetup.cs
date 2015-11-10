f2defea7df7838b57c0d1739093beff7e61017af
Hayden Platt <misterplatt17@gmail.com>
2015-11-10 15:06:20 -0800

    Initial mobile testing

---
+++ Shackle/Assets/Scripts/Player_NetworkSetup.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Player_NetworkSetup : NetworkBehaviour {
+
+	[SerializeField] Camera CharacterCam;
+	[SerializeField] AudioListener audioListener;
+
+	// Use this for initialization
+	void Start () {
+		if (isLocalPlayer) {
+			GameObject.Find("Scene Camera").SetActive(false);
+			GetComponent<CharacterController>().enabled = true;
+			GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enabled = true;
+			CharacterCam.enabled = true;
+			audioListener.enabled = true;
+		}
+	}
+
+}
---
+++ Shackle/Assets/Scripts/reticle.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class reticle : MonoBehaviour {
+
+	Ray ray;
+	RaycastHit hit;
+
+	public GameObject invkey;
+	public inventory inventoryManager;
+
+	// Use this for initialization
+	void Start () {
+		invkey = GameObject.Find ("key_icon");
+		invkey.SetActive (false);
+		inventoryManager = GameObject.Find ("Inventory").GetComponent<inventory> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ray = Camera.main.ScreenPointToRay(new Vector3(Camera.main.pixelWidth/2, Camera.main.pixelHeight/2, 0));
+
+		if(Physics.Raycast(ray, out hit)){
+			Debug.Log(hit.collider.name);
+
+			//Change reticle color when object is interactable
+			if(hit.collider.tag == "Interactable"){
+				this.GetComponent<Image>().color = Color.green;
+			} else {
+				this.GetComponent<Image>().color = Color.white;
+			}
+
+			//Specific object actions
+			if(Input.GetMouseButtonDown(0)){
+				//If the player click's on the door, open around parent pivot
+				if(hit.collider.name == "door"){
+					Debug.Log("OPEN SESAME BITCH");
+					hit.transform.parent.Rotate(Vector3.up, 90);
+				}
+
+				//If click on key, destroy world instance and add to inventory
+				if(hit.collider.name == "key"){
+					Debug.Log("Got a key!");
+					inventoryManager.addItem(hit.collider.name);
+					invkey.SetActive (true);
+					Destroy(hit.transform.gameObject);
+				}
+			}
+		}
+	}
+}
